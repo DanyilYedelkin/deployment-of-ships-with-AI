16758716655fb180ae983de8ecfefc971aa2a2b8
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-11-25 13:50:20 +0100

    add buttons and the ability to use the mouse with the correct coordinates

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import time
+import random
 
 
 tk = Tk()
@@ -16,6 +17,14 @@ size_canvas_y = step_y * s_y
 
 menu_x = 250
 
+ships = 10 # max count of ships
+ship_len1 = 1 # the length for the 1th type of ships
+ship_len2 = 2 # the length for the 2th type of ships
+ship_len3 = 3 # the length for the 3th type of ships
+ship_len4 = 4 # the length for the 4th type of ships
+
+enemy_ships = [[0 for i in range(s_x+1)] for i in range(s_y+1)]
+
 
 
 
@@ -25,12 +34,13 @@ def on_closing():
         app_running = False
         tk.destroy()
 
+
 tk.protocol("WM_DELETE_WINDOW", on_closing)
 tk.title("Lode game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=size_canvas_x+menu_x, height=size_canvas_y, bd=0, highlightthickness=0)
-canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="white")
+canvas = Canvas(tk, width=size_canvas_x+menu_x, height=size_canvas_y, bg='#292929', bd=0, highlightthickness=0)
+canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="#3D3D3D")
 canvas.pack()
 tk.update()
 
@@ -52,11 +62,43 @@ def button_generate_map():
     pass
 
 
-b0 = Button(tk, text="Generate locations for ships", command = button_generate_ships)
+b0 = Button(tk, bg='#7C7CFC', text="Generate locations for ships", command = button_generate_ships)
 b0.place(x = size_canvas_x+20, y = 30)
 
-b1 = Button(tk, text="Generate the map again!", command = button_generate_map)
+b1 = Button(tk, bg='#9090EE', text="Generate the map again!", command = button_generate_map)
 b1.place(x = size_canvas_x+20, y = 70)
+
+
+def add_to_all(event):
+    _type = 0 # for LMB
+    if event.num == 3:
+        _type = 1 # for RMB
+    mouse_x = canvas.winfo_pointerx() - canvas.winfo_rootx()
+    mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
+    ip_x = mouse_x // step_x
+    ip_y = mouse_y // step_y
+    print (ip_x, ip_y, "_type:", _type)
+    
+
+
+canvas.bind_all("<Button-1>", add_to_all) # left button of mouse (LMB)
+canvas.bind_all("<Button-3>", add_to_all) # right button of mouse (RMB)
+
+
+def generate_ships():
+    global enemy_ships
+    ships_list = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+    sum_1_all_ships = sum(ships_list)
+    sum_1_enemy = 0;
+    
+    
+    
+    
+    #while sum_1_enemy != sum_1_all_ships:
+    #    pass
+
+
+generate_ships()
 
 
 while app_running:
