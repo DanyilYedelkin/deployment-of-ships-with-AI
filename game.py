6d289172a4036eb1e44804819f85602fbe308ad2
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2022-12-06 15:34:14 +0100

    change map (make it as in the example (always true)

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
 import time
-#import random
 from random import *
 
 
@@ -47,13 +46,39 @@ canvas = Canvas(tk, width=size_canvas_x+menu_x, height=size_canvas_y, bg='#29292
 
 def generate_map(): 
     # add numbers into the map
-    for i in range(0, s_x):
-        number = randint(0, 8)
-        game_map[i+1][0] = number
+    #for i in range(0, s_x):
+    #    number = randint(0, 8)
+    #    game_map[i+1][0] = number
 
-    for i in range(0, s_y):
-        number = randint(0, 8)
-        game_map[0][i+1] = number 
+    #for i in range(0, s_y):
+    #    number = randint(0, 8)
+    #    game_map[0][i+1] = number
+    
+    # for X coordinate
+    game_map[1][0] = 4
+    game_map[2][0] = 2
+    game_map[3][0] = 0
+    game_map[4][0] = 4
+    game_map[5][0] = 2
+    game_map[6][0] = 3
+    game_map[7][0] = 0
+    game_map[8][0] = 0
+    game_map[9][0] = 5
+    game_map[10][0] = 0
+
+    
+    # for Y coordinate
+    game_map[0][1] = 6
+    game_map[0][2] = 1
+    game_map[0][3] = 2
+    game_map[0][4] = 1
+    game_map[0][5] = 0
+    game_map[0][6] = 2
+    game_map[0][7] = 3
+    game_map[0][8] = 0
+    game_map[0][9] = 1
+    game_map[0][10] = 4
+    
         
     global canvas
     canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="#3D3D3D")
